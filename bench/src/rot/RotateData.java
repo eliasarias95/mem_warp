5344e3c1053849ca36a8fd2ce31016e77f0ea21f
Elias Arias <elias.arias550@gmail.com>
2017-04-27 20:36:10 -0600

    Added slope estimation functionality. 	modified:   src/rot/RotateData.java

--- bench/src/rot/RotateData.java
+++ bench/src/rot/RotateData.java
@@ -3,6 +3,7 @@ package rot;
 import util.*;
 
 import edu.mines.jtk.dsp.*;
+import slopes.*;
 
 import static edu.mines.jtk.util.ArrayMath.*;
 
@@ -33,7 +34,7 @@ public class RotateData {
     _dwkS = new DynamicWarpingK(k,shift_min,shift_max,s2,s1);
     _dwkS.setSmoothness(sub_samp2,sub_samp1);
     _dwkS.setStrainLimits(strain2min,strain2max,strain1min,strain1max);
-  }
+      }
 
   public float[][] computeTimeShifts(float[][] ref, float[][] rot) {
     float[][] shifts = _dwkT.findShifts(_s1,ref,_s1,rot);
@@ -89,8 +90,8 @@ public class RotateData {
         double shift_min = -5.0; // max shift allowed
         double shift_max =  5.0; // max shift allowed
 
-        double sub_samp1 = 200.0; // bigger = more smooth
-        double sub_samp2 = 60.0; // smaller = less smooth
+        double sub_samp1 = 10.0; // bigger = more smooth
+        double sub_samp2 = 3.0; // smaller = less smooth
 
         double strain1min = -0.2; // min strain in 1st dim
         double strain2min = -0.3; // min strain in 2nd dim
@@ -101,15 +102,38 @@ public class RotateData {
         Sampling ss2 = new Sampling(n2);
         RotateData rd = new RotateData(k,ss1,ss2,shift_min,shift_max,
             sub_samp1,sub_samp2,strain1min,strain1max,strain2min,strain2max);
+        DynamicWarpingSlopes dws = new DynamicWarpingSlopes(k,shift_max,
+            sub_samp1,sub_samp2,strain1max,strain2max,ss1,ss2);
 
         // Read data in
         int i = 12; // values -12 to 12 for now
         float[][] reference = Utility.readL(n1,n2,
-          System.getProperty("user.home") + 
+            System.getProperty("user.home") + 
             "/home/git/mem_warp/bench/data/eliasData0.rsf@");
         float[][] data = Utility.readL(n1,n2,
-          System.getProperty("user.home") + 
+            System.getProperty("user.home") + 
             "/home/git/mem_warp/bench/data/eliasData"+i+".rsf@");
+
+        // slope estimation
+        //float[][] slope_ref = Utility.readL(n1,n2,
+        // System.getProperty("user.home") + 
+        //   "/home/git/mem_warp/bench/data/eliasData0_slope.rsf@");
+        //float[][] slope_data = Utility.readL(n1,n2,
+        //  System.getProperty("user.home") + 
+        //    "/home/git/mem_warp/bench/data/eliasData"+i+"_slope.rsf@");
+
+        float[][] slope_ref = new float[n2][n1];
+        float[][] slope_data = new float[n2][n1];
+        dws.findSmoothSlopes(ss1,reference,slope_ref);
+        dws.findSmoothSlopes(ss1,data,slope_data);
+        Utility.writeL(slope_ref,System.getProperty("user.home") + 
+            "/home/git/mem_warp/bench/data/eliasData0_slope.rsf@");
+        Utility.writeL(slope_data,System.getProperty("user.home") + 
+            "/home/git/mem_warp/bench/data/eliasData"+i+"_slope.rsf@");
+
+        // getting proper units for plotting
+        slope_ref = mul((float)(d1/d2),slope_ref);
+        slope_data = mul((float)(d1/d2),slope_data);
 
         // compute time & spatial shifts
         float[][] timeShifts = rd.computeTimeShifts(reference,data);
@@ -151,6 +175,20 @@ public class RotateData {
             (float)shift_min,(float)shift_max,paint);
         Plot.plot(s1,s2,data,timeShifts,"Time shifts","Shift (ms)",
             (float)shift_min,(float)shift_max,paint);
+
+        // computed slopes
+        Plot.plot(s1,s2,data,slope_data,"Angled data slopes",
+            "slope (s/km)",(float)shift_min,(float)shift_max,paint);
+        Plot.plot(s1,s2,reference,slope_ref,"Flat data slopes",
+            "slope (s/km)",(float)shift_min,(float)shift_max,paint);
+
+        // slope difference
+        cmin = -4.0f;
+        cmax = 4.0f;
+        color = true;
+        showColorbar = true;
+        Plot.plot(s1,s2,sub(slope_data,slope_ref),"Difference between slopes",
+            "slope (s/km)",cmin,cmax,color,paint,showColorbar);
       }
     });
   }
